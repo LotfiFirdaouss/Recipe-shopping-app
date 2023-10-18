21e5b51ac1ba6f88c29f0a5472b530a5c383ba6b
Firdaouss LOTFI <flotfi@astekgroup.ma>
2023-10-18 14:26:49 +0100

    OPTIMIZINT THE APPLICATION -- splitting the project to moduules + using lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,20 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
-import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/Recipes', pathMatch: 'full' },
-
     { 
-        path: 'Recipes', 
-        component: RecipesComponent,
-        canActivate: [AuthGuard],
-        children: [
-            { path: '', component: RecipeStartComponent },
-            { path: 'new', component: RecipeEditComponent },
-            { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-            { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
-        ]
+        path:'Recipes',
+        loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)
     },
-    
-    { path: 'Shopping', component: ShoppingListComponent },
-
-    { path: 'auth', component: AuthComponent }
+    { 
+        path:'Shopping',
+        loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)
+    },
+    { 
+        path:'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    }
 ];
 
 @NgModule({
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,7 +14,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy{
   private subscription: Subscription;
 
   constructor(
-    private shoppingListService: ShoppingListService
+    private shoppingListService: ShoppingListService,
+    private loggingService: LoggingService
   ){}
 
   ngOnInit() {
@@ -23,6 +25,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy{
         this.ingredients = ingredients;
       }
     );
+    this.loggingService.printLog('Hello from shoppinglist component ngOnInit');
   }
 
   ngOnDestroy(): void {
